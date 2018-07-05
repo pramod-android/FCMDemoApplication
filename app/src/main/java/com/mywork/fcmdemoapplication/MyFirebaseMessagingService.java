7cfbe9f652532d3c580e9b74c96f39d297fb61de
Santosh Patil <patilsantoshdilip@gmail.com>
2018-07-05 09:52:06 +0530

    Initial commit

---
+++ app/src/main/java/com/mywork/fcmdemoapplication/MyFirebaseMessagingService.java
@@ -0,0 +1,43 @@
+package com.mywork.fcmdemoapplication;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String message = remoteMessage.getNotification().getBody();
+        DisplayNotification(this,message);
+
+    }
+
+    public void DisplayNotification(Context context, String message){
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(context, Main2Activity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, "Default")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("My notification")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, mBuilder.build());
+    }
+}
